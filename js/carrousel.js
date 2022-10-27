1f684f352ace940e81c9b8e4694b28aea8638ccf
Rizo <g.rizzoli@hotmail.ca>
2022-10-27 16:36:39 -0400

    recherche de l'aatribut source de la galerie

--- js/carrousel.js
+++ js/carrousel.js
@@ -1,15 +1,21 @@
 (function(){
     console.log("carrousel");
+    let elmGalerie = document.querySelector('.galerie');
+    let elmGalerieImg = document.querySelectorAll('.galerie img');
     let elmBouton = document.querySelector('.bouton');
     let elmCarrousel = document.querySelector('.carrousel');
     let elmCarrousel__x = document.querySelector('.carrousel__x');
+
     elmBouton.addEventListener('mousedown', function(){
         console.log("ouverture du carrousel!");
-        elmCarrousel.classList.add("carrousel--ouvrir");
+        elmCarrousel.classList.add('carrousel--ouvrir');
+        for(const elmImg of elmGalerieImg){
+            console.log(elmImg.getAttribute('src'));
+        }
     })
 
     elmCarrousel__x.addEventListener('mousedown', function(){
-        elmCarrousel.classList.remove("carrousel--ouvrir");
+        elmCarrousel.classList.remove('carrousel--ouvrir');
     })
 
 })();

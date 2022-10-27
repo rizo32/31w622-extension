83dfc2fe7ddd0dd19cd5770b6cffc5ba3c562e4c
Rizo <g.rizzoli@hotmail.ca>
2022-10-27 14:21:04 -0400

    contenu du carousel

--- rizo_carrousel.php
+++ rizo_carrousel.php
@@ -13,21 +13,20 @@
 Plugin Name: rizo_carrousel
 Description: Afficher une boite modale de l'image sélectionnée d'une galerie pour ensuite accéder à toutes les images de la galerie.
 Author: Gabriel R. Rizzoli
-Author URI: https://github.com/ridz32
+Author URI: https://github.com/rizo32
 Version: 1.0.0
 */
 
-function genere_html(){
+function boite_carrousel(){
     /////////////////////////////////////// HTML
     // Le conteneur d'une boîte
     $contenu = 
-      "<div class='boite'>"
-    . "<code>......Auteur: " . get_the_author() . "</code>"
-    . "<date>......Date de publication: " . get_the_date() . "</date>"
-    . "<h5>......Adresse URL" . get_the_guid() . "</h5>"
-    . "<h6>......Catégorie: " . get_the_category() . "</h6>"
-    . '</div> <!-- fin class="boite" -->';
+      `<section class='carrousel'>
+      <button class="carrousel--fermer>X<button>
+      <figure class="carrousel__figure></figure>
+      <form class="carrousel__form"></form>
+      </section><!-- fin class="carrousel" -->`;
     
     return $contenu;
    }
-   add_shortcode('mon_html', 'genere_html');
+   add_shortcode('mon_html', 'boite_carrousel');

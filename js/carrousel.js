c26aecfc1ba72b46351fefc26118800b309b4015
Rizo <g.rizzoli@hotmail.ca>
2022-11-02 14:21:32 -0400

    implementation des boutons radio

--- js/carrousel.js
+++ js/carrousel.js
@@ -8,21 +8,37 @@
     let elmCarrousel = document.querySelector('.carrousel');
     let elmCarrousel__x = document.querySelector('.carrousel__x');
     let elmCarrousel__figure = document.querySelector('.carrousel__figure');
+    let elmCarrousel__form = document.querySelector('.carrousel__form');
     let index = 0;
+    let dernierIndex;
+
 
     elmBouton.addEventListener('mousedown', function(){
-        console.log("ouverture du carrousel!");
         elmCarrousel.classList.add('carrousel--ouvrir');
         for(const elmImg of elmGalerieImg){
             ajout_img_dans_carrousel(elmImg);
-            ajout_radio_dans_carrousel();           
+            ajout_radio_dans_carrousel();  
         }
     })
 
     function ajout_radio_dans_carrousel(){
-        let elmCarrouselFormRadio = document.createElement(input);
-        elmCarrouselFormRadio.setAttribute('type', 'radio');
-        elmCarrouselFormRadio.classList.add('carrousel__form__rad', 'radio');
+        let elmCarrousel__form__radio = document.createElement("input");
+        elmCarrousel__form__radio.setAttribute('type', 'radio');
+        elmCarrousel__form__radio.classList.add('carrousel__form__rad', 'radio');
+        elmCarrousel__form__radio.setAttribute('name', 'carrousel__form__rad');
+
+        elmCarrousel__form__radio.dataset.index = index;
+        index ++;
+        elmCarrousel__form.appendChild(elmCarrousel__form__radio);
+        elmCarrousel__form__radio.addEventListener('mousedown', function(){
+            if(dernierIndex){
+                log(this.dataset.index);
+                elmCarrousel__figure.children[dernierIndex].classList.remove("carrousel__figure__img--activer");
+                dernierIndex = this.dataset.index;
+            } 
+                elmCarrousel__figure.children[dataset.index].classList.add("carrousel__figure__img--activer");
+                dernierIndex = this.dataset.index;
+        });
     }
 
     function ajout_img_dans_carrousel(elmImg) {

9dd8e6bcd1b9421188dae2f7397edb7370a82e28
Rizo <g.rizzoli@hotmail.ca>
2022-11-02 13:46:48 -0400

    ajustement de la talle des images du carrousel

--- js/carrousel.js
+++ js/carrousel.js
@@ -8,19 +8,29 @@
     let elmCarrousel = document.querySelector('.carrousel');
     let elmCarrousel__x = document.querySelector('.carrousel__x');
     let elmCarrousel__figure = document.querySelector('.carrousel__figure');
+    let index = 0;
 
     elmBouton.addEventListener('mousedown', function(){
         console.log("ouverture du carrousel!");
         elmCarrousel.classList.add('carrousel--ouvrir');
         for(const elmImg of elmGalerieImg){
-            let elmCarrouselImg = document.createElement('img');
-            elmCarrouselImg.setAttribute('src', elmImg.getAttribute('src'));
-            elmCarrousel.classList.add('carrousel__figure__img');
-            elmCarrousel__figure.appendChild(elmCarrouselImg);
-
-            
+            ajout_img_dans_carrousel(elmImg);
+            ajout_radio_dans_carrousel();           
         }
     })
+
+    function ajout_radio_dans_carrousel(){
+        let elmCarrouselFormRadio = document.createElement(input);
+        elmCarrouselFormRadio.setAttribute('type', 'radio');
+        elmCarrouselFormRadio.classList.add('carrousel__form__rad', 'radio');
+    }
+
+    function ajout_img_dans_carrousel(elmImg) {
+        let elmCarrouselImg = document.createElement('img');
+        elmCarrouselImg.setAttribute('src', elmImg.getAttribute('src'));
+        elmCarrousel.classList.add('carrousel__figure__img');
+        elmCarrousel__figure.appendChild(elmCarrouselImg);
+    }
 
     elmCarrousel__x.addEventListener('mousedown', function(){
         elmCarrousel.classList.remove('carrousel--ouvrir');
